a31b85654117b0bf4deecaba242d2851f82138be
Katie Simpson <Katie.Simpson@ecan.govt.nz>
2019-04-26 09:08:22 +1200

    Add Data
    Added and Shaped Data for Report.
    
    Still issues with linking Inspections to Alerts. Will cleanse Alerts and reassess impact.

--- WUA final report.py
+++ WUA final report.py
@@ -6,7 +6,7 @@ Created on Fri Mar 29 11:34:51 2019
 
 WUA Final Report
 
-Output tables e-mailed to Carly Waddleton Eila Gendig
+Output tables e-mailed to Regional Implementation
 """
 
 # Import packages
@@ -20,10 +20,10 @@ RunDate = datetime.now()
 
 
 # Input Parameters
-InspCols = [
+InspectionCol = [
         'InspectionID',
         'B1_ALT_ID',
-        'Subtype',
+        #'Subtype',
         'InspectionStatus',
         'InspectionCompleteDate',
         'R3_DEPTNAME',
@@ -33,7 +33,19 @@ InspCols = [
         'StatusDate'
         ]
 
-AlertCols = [
+InspectionColNames = {
+        'B1_ALT_ID': 'ConsentNo',
+        'R3_DEPTNAME': 'MonitoringDepartment',
+        'REC_FUL_NAM': 'RMOA'
+        }
+
+InspectionImportFilter = {
+        'Subtype': ['Water Use - Alert']
+        }
+
+
+
+AlertCol = [
         'ID',
         'ConsentID or Water Group',
         'Meters',
@@ -49,14 +61,166 @@ AlertCols = [
         'LastModifiedDate'
         ]
 
-AlertStatusCols = [
+AlertColNames = {
+        'ID' : 'AlertID',
+        'ConsentID or Water Group': 'ConsentNo'
+        }
+
+AlertImportFilter = {
         
-        
-        
-        
+        }
+
+
+
+AlertStatusCol = [  
+        'ID',
+        'Description'
         ]
 
+AlertStatusColNames = {
+        'ID' : 'StatusID',
+        'Description' : 'Status'
+        }
+
+AlertStatusImportFilter = {
+        
+        }
+
+
+
+IgnoredConsentsCol = [
+       # 'ID',
+        'ConsentID_or_Water_Group',
+       # 'IgnoreReasonID',
+       # 'Comment',
+       # 'CreatedBy',
+       # 'StartDate',
+       # 'EndDate',
+        'ReasonID'
+        ]
+
+
+IgnoredConsentsColNames = {
+        'ConsentID_or_Water_Group': 'ConsentNo',
+        'ReasonID': 'IgnoreType'
+        }
+
+IgnoredConsentsImportFilter = {
+        
+        }
+
+# Query SQL tables : FEP Individual
+#Inspection = pdsql.mssql.rd_sql(
+#                   'SQL2012PROD03',
+#                   'DataWarehouse', 
+#                   table = 'D_ACC_FEP_IndividualSummary',
+#                   col_names = AuditCol,
+#                   where_op = 'AND',
+#                   where_in = AlertFilter,
+#                   date_col = 'AuditDate',
+#                   from_date = '2018-07-01'
+#                   )
+
+# Query SQL tables : FEP Individual
+Inspection = pdsql.mssql.rd_sql(
+                   'SQL2012PROD03',
+                   'DataWarehouse', 
+                   table = 'D_ACC_Inspections',
+                   col_names = InspectionCol,
+                   where_op = 'AND',
+                   where_in = InspectionImportFilter,
+                   date_col = 'StatusDate',
+                   from_date = '2019-01-01'
+                   )
+Inspection.rename(columns=InspectionColNames, inplace=True)
+
+
+Alert = pdsql.mssql.rd_sql(
+                   'SQL2012Test01',
+                   'Hilltop', 
+                   table = 'DailyAlert',
+                   col_names = AlertCol
+                   )
+#NULL imports as 'None' but reacts like NULL in groupby
+# Alert.groupby(['Username'])['ID'].aggregate('count')
+Alert.rename(columns=AlertColNames, inplace=True)
+
+AlertStatus = pdsql.mssql.rd_sql(
+                   'SQL2012Test01',
+                   'Hilltop', 
+                   table = 'DailyAlertStatus',
+                   col_names = AlertStatusCol,
+                   )
+
+AlertStatus.rename(columns=AlertStatusColNames, inplace=True)
+
+IgnoredConsents = pdsql.mssql.rd_sql(
+                   'SQL2012Test01',
+                   'Hilltop', 
+                   table = 'DailyAlertConsentsToFilter',
+                   col_names = IgnoredConsentsCol
+                   )
+IgnoredConsents.rename(columns=IgnoredConsentsColNames, inplace=True)
+IgnoredConsents.groupby(['IgnoreType'])['ConsentNo'].aggregate('count')
+
+# linking status ID
+Alert = pd.merge(Alert, AlertStatus, on ='StatusID', how = 'left')
+
+Alert.groupby(['Status'])['AlertID'].aggregate('count')
+
+#Pull out Processed Alerts and Inspection Numbers
+Processed = Alert[Alert['Username'].notnull()]
+
+Processed['InspectionID'] = Processed['Comment'].str[:7]
 #
+Processed['InspectionID']= pd.to_numeric(Processed['InspectionID'],errors='coerce', downcast='integer')
+
+WUAInspections = Processed['InspectionID'].dropna()
+
+WUAInspections_List = WUAInspections.values.tolist()
+
+#Import Inspections created by Alerts Process
+LinkedInspection = pdsql.mssql.rd_sql(
+                   'SQL2012PROD03',
+                   'DataWarehouse', 
+                   table = 'D_ACC_Inspections',
+                   col_names = InspectionCol,
+                   where_in = {'InspectionID': WUAInspections_List}
+                   )
+LinkedInspection.rename(columns=InspectionColNames, inplace=True)
+
+
+Processed = pd.merge(Processed, LinkedInspection, on ='InspectionID', how = 'left')
+
+
+
+
+
+Processed.groupby(['Status'])['AlertID'].aggregate('count')
+# Linking Ignored Consents
+Alert = pd.merge(Alert, IgnoredConsents, on ='ConsentNo', how = 'left')
+
+Alert.groupby(['IgnoreType'])['AlertID'].aggregate('count')
+
+#linking Inspection
+Inspection.count()
+Alert.groupby(['Status'])['AlertID'].aggregate('count')
+
+
+
+
+
+
+
+#x = pd.merge(LinkedInspection, Processed, on ='InspectionID', how = 'left')
+#
+#
+#
+#x.to_csv('test.csv')
+
+
+
+
 #WUAInspectionsFilter = {
 #        'InspectionStatus': ['In process'],
 #        'Subtype': ['Water Use - Alert']
